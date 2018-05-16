174d875e80685d74aa65f954634c2448e1f2f0da
katyarun@gmail.com <katyarun@gmail.com>
2018-05-16 00:55:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,4 +4,9 @@ function updatePlaylist(playlist, artist, song) {
   return Object.assign(playlist, {[artist] : song});
 }
 
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist;
+  return playlist;
+}
+
 console.log(updatePlaylist(playlist, '1 2', 'its song'));
