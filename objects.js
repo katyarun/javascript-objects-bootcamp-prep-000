186fd50c95cdd577c2f4dc3e15f1f2ff7fc2ad7c
katyarun@gmail.com <katyarun@gmail.com>
2018-05-16 00:51:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,5 @@ var playlist = { 'antonio vivaldi': 'the 4 seasons', 'seb bach': 'Allegro'};
 function updatePlaylist(playlist, artist, song) {
   return Object.assign({}, playlist, {[artist] : song});
 }
+
+updatePlaylist(playlist, '1 2', 'its song');
